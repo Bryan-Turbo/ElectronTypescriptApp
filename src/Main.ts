a6d1327418290ad268bfa57847f62de2033443ae
Bryan-Turbo <bryank.b96@gmail.com>
2017-07-22 01:25:21 +0200

    added simple 'hello' application

---
+++ src/Main.ts
@@ -0,0 +1,29 @@
+import { BrowserWindow } from 'electron';
+export default class Main {
+    static mainWindow: Electron.BrowserWindow;
+    static application: Electron.App;
+    static BrowserWindow;
+
+
+
+    static main(app: Electron.App, browserWindow: typeof BrowserWindow) {
+        Main.application = app;
+        Main.BrowserWindow = browserWindow;
+
+        Main.application.on('window-all-closed', function () {
+            if (process.platform !== 'darwin') {
+                app.quit()
+            }
+        });
+
+        Main.application.on('ready', function () {
+            Main.mainWindow = new BrowserWindow({ width: 400, height: 300 });
+
+            //Yes, I know this hardcoded bit is bad, but it was late
+            Main.mainWindow.loadURL(`file://${__dirname}/../Views/index.html`);
+            Main.mainWindow.on('close', function () {
+                Main.mainWindow = null;
+            })
+        });
+    }
+}
